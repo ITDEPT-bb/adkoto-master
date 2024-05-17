eba6be38587ee55e0a1cfad6e402ee798717ba1d
TianTianny <acdeocera.bb88@gmail.com>
2024-05-17 11:18:46 +0800

    Use AuthenticatedLayout on home to display navbar

--- resources/js/Pages/Tribekoto/Home.vue
+++ resources/js/Pages/Tribekoto/Home.vue
@@ -4,26 +4,29 @@ import GroupList from "@/Components/Tribekoto/GroupList.vue";
 import FollowingList from "@/Components/Tribekoto/FollowingList.vue";
 import CreatePost from "@/Components/Tribekoto/CreatePost.vue";
 import PostList from "@/Components/Tribekoto/PostList.vue";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 defineProps({});
 </script>
 
 <template>
     <Head title="Social Media Website" />
 
-    <div class="grid lg:grid-cols-12 gap-3 p-4 h-full">
-        <div class="lg:col-span-3 lg:order-1 h-full overflow-hidden">
-            <GroupList />
+    <AuthenticatedLayout>
+        <div class="grid lg:grid-cols-12 gap-3 p-4 h-full">
+            <div class="lg:col-span-3 lg:order-1 h-full overflow-hidden">
+                <GroupList />
+            </div>
+            <div class="lg:col-span-3 lg:order-3 h-full overflow-hidden">
+                <FollowingList />
+            </div>
+            <div
+                class="lg:col-span-6 lg:order-2 h-full overflow-hidden flex flex-col"
+            >
+                <CreatePost />
+                <PostList class="flex-1" />
+            </div>
         </div>
-        <div class="lg:col-span-3 lg:order-3 h-full overflow-hidden">
-            <FollowingList />
-        </div>
-        <div
-            class="lg:col-span-6 lg:order-2 h-full overflow-hidden flex flex-col"
-        >
-            <CreatePost />
-            <PostList class="flex-1" />
-        </div>
-    </div>
+    </AuthenticatedLayout>
 </template>
 
 <style></style>
