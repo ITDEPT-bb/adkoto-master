23467d757dc2308af9ec35beab0245432e6fa8be
TianTian-Dev <ahllainedev@gmail.com>
2024-05-18 00:03:03 +0800

    Create new PostAttachments component

--- resources/js/Components/Tribekoto/PostItem.vue
+++ resources/js/Components/Tribekoto/PostItem.vue
@@ -1,24 +1,16 @@
 <script setup>
-import {
-    ChatBubbleLeftRightIcon,
-    ChatBubbleLeftEllipsisIcon,
-    HandThumbUpIcon,
-    ArrowDownTrayIcon
-} from '@heroicons/vue/24/outline'
+import { ChatBubbleLeftEllipsisIcon, ChatBubbleLeftRightIcon, HandThumbUpIcon } from '@heroicons/vue/24/outline'
 
 import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
 import PostUserHeader from "@/Components/Tribekoto/PostUserHeader.vue";
-import { router, usePage } from '@inertiajs/vue3'
-import { isImage } from "@/helpers.js";
-import { PaperClipIcon } from "@heroicons/vue/24/solid/index.js";
+import { router, usePage } from '@inertiajs/vue3';
 import axiosClient from "@/axiosClient.js";
 import InputTextarea from "@/Components/InputTextarea.vue";
-import PrimaryButton from "@/Components/PrimaryButton.vue";
 import IndigoButton from "@/Components/Tribekoto/IndigoButton.vue";
 import { ref } from "vue";
 import ReadMoreReadLess from "@/Components/Tribekoto/ReadMoreReadLess.vue";
 import EditDeleteDropdown from "@/Components/Tribekoto/EditDeleteDropdown.vue";
-import DangerButton from "@/Components/DangerButton.vue";
+import PostAttachments from "@/Components/Tribekoto/PostAttachments.vue";
 
 const authUser = usePage().props.auth.user;
 const editingComment = ref(null);
@@ -122,30 +114,10 @@ function deletePost() {
         <div class="grid gap-3 mb-3" :class="[
             post.attachments.length === 1 ? 'grid-cols-1' : 'grid-cols-2',
         ]">
-            <template v-for="(attachment, ind) of post.attachments.slice(0, 4)">
-                <div @click="openAttachment(ind)"
-                    class="group aspect-square bg-blue-100 flex flex-col items-center justify-center text-gray-500 relative cursor-pointer">
-                    <div v-if="ind === 3 && post.attachments.length > 4"
-                        class="absolute left-0 top-0 right-0 bottom-0 z-10 bg-black/60 text-white flex items-center justify-center text-2xl">
-                        +{{ post.attachments.length - 4 }} more
-                    </div>
-                    <!-- Download-->
-                    <a @click.stop :href="route('post.download', attachment)"
-                        class="z-20 opacity-0 group-hover:opacity-100 transition-all w-8 h-8 flex items-center justify-center text-gray-100 bg-gray-700 rounded absolute right-2 top-2 cursor-pointer hover:bg-gray-800">
-                        <ArrowDownTrayIcon class="w-4 h-4" />
-                    </a>
-                    <!--/ Download-->
-
-                    <img v-if="isImage(attachment)" :src="attachment.url" class="object-contain aspect-square" />
-                    <div v-else class="flex flex-col justify-center items-center">
-                        <PaperClipIcon class="w-10 h-10 mb-3" />
-
-                        <small>{{ attachment.name }}</small>
-                    </div>
-                </div>
-            </template>
+            <PostAttachments :attachments="post.attachments" @attachmentClick="openAttachment" />
         </div>
         <Disclosure v-slot="{ open }">
+            <!--Like & Comment buttons-->
             <div class="flex gap-2">
                 <button @click="sendReaction"
                     class="text-gray-800 flex gap-1 items-center justify-center  rounded-lg py-2 px-4 flex-1" :class="[
@@ -234,5 +206,3 @@ function deletePost() {
         </Disclosure>
     </div>
 </template>
-
-<style scoped></style>
