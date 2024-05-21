830a95f8becebccc5b725d135571064caf6b3090
TianTianny <acdeocera.bb88@gmail.com>
2024-05-21 15:09:05 +0800

    update on loading more post

--- resources/js/Pages/Tribekoto/Home.vue
+++ resources/js/Pages/Tribekoto/Home.vue
@@ -18,7 +18,7 @@ defineProps({
     <Head title="Social Media Website" />
 
     <AuthenticatedLayout>
-        <div class="grid lg:grid-cols-12 gap-3 p-4 h-full">
+        <div class="grid max-w-7xl mx-auto lg:grid-cols-12 gap-3 p-4 h-full">
             <div class="lg:col-span-3 lg:order-1 h-full overflow-hidden">
                 <GroupList :groups="groups" />
             </div>
