24e308b52464b10d21e7f83423097fec8ccb3e06
TianTianny <acdeocera.bb88@gmail.com>
2024-06-21 10:49:43 +0800

    changing the delete notif for delete post and delete ads

--- resources/js/Components/Adkoto/AdCardItems.vue
+++ resources/js/Components/Adkoto/AdCardItems.vue
@@ -26,8 +26,14 @@
                 >
                     Edit
                 </button>
-                <button
+                <!-- <button
                     @click="confirmDelete(ad.id)"
+                    class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
+                >
+                    Delete
+                </button> -->
+                <button
+                    @click="openDeleteModalAds"
                     class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
                 >
                     Delete
@@ -43,28 +49,145 @@
                 Delete
             </button>
         </div> -->
+        <!-- Delete Modal -->
+        <TransitionRoot as="template" :show="open">
+            <Dialog class="relative z-10" @close="open = false">
+                <TransitionChild
+                    as="template"
+                    enter="ease-out duration-300"
+                    enter-from="opacity-0"
+                    enter-to="opacity-100"
+                    leave="ease-in duration-200"
+                    leave-from="opacity-100"
+                    leave-to="opacity-0"
+                >
+                    <div
+                        class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+                    />
+                </TransitionChild>
+
+                <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+                    <div
+                        class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0"
+                    >
+                        <TransitionChild
+                            as="template"
+                            enter="ease-out duration-300"
+                            enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                            enter-to="opacity-100 translate-y-0 sm:scale-100"
+                            leave="ease-in duration-200"
+                            leave-from="opacity-100 translate-y-0 sm:scale-100"
+                            leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        >
+                            <DialogPanel
+                                class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg"
+                            >
+                                <div
+                                    class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4"
+                                >
+                                    <div class="sm:flex sm:items-start">
+                                        <div
+                                            class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10"
+                                        >
+                                            <ExclamationTriangleIcon
+                                                class="h-6 w-6 text-red-600"
+                                                aria-hidden="true"
+                                            />
+                                        </div>
+                                        <div
+                                            class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left"
+                                        >
+                                            <DialogTitle
+                                                as="h3"
+                                                class="text-base font-semibold leading-6 text-gray-900"
+                                            >
+                                                Delete Advertisement
+                                            </DialogTitle>
+                                            <div class="mt-2">
+                                                <p
+                                                    class="text-sm text-gray-500"
+                                                >
+                                                    Are you sure you want to
+                                                    delete this Advertisement?
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div
+                                    class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6"
+                                >
+                                    <button
+                                        type="button"
+                                        class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                                        @click="confirmDelete"
+                                    >
+                                        Delete
+                                    </button>
+                                    <button
+                                        type="button"
+                                        class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                                        @click="open = false"
+                                        ref="cancelButtonRef"
+                                    >
+                                        Cancel
+                                    </button>
+                                </div>
+                            </DialogPanel>
+                        </TransitionChild>
+                    </div>
+                </div>
+            </Dialog>
+        </TransitionRoot>
     </div>
 </template>
 
 <script setup>
-import { defineProps } from "vue";
-import { Link } from "@inertiajs/vue3";
+import { defineProps, ref } from "vue";
+import { router } from "@inertiajs/vue3";
+import { MapPinIcon } from "@heroicons/vue/24/outline/index.js";
+import {
+    Dialog,
+    DialogPanel,
+    DialogTitle,
+    TransitionChild,
+    TransitionRoot,
+} from "@headlessui/vue";
+import { ExclamationTriangleIcon } from "@heroicons/vue/24/outline";
+
+const open = ref(false);
+
+const openDeleteModalAds = () => {
+    open.value = true;
+};
 
 const props = defineProps({
     ad: Object,
 });
+
+const confirmDelete = () => {
+    router
+        .delete(route("adkoto.destroy", props.ad.id), {
+            preserveScroll: true,
+        })
+        .then(() => {
+            open.value = false;
+        })
+        .catch((error) => {
+            console.error("Failed to delete post:", error);
+        });
+};
 
 const editAd = (ad) => {
     // Implement logic to open a modal for editing the ad
     console.log(`Editing ad with ID ${ad.id}`);
 };
 
-const confirmDelete = (adId) => {
-    if (confirm(`Are you sure you want to delete ad with ID ${adId}?`)) {
-        // Implement logic to delete the ad (usually via API call)
-        console.log(`Deleting ad with ID ${adId}`);
-    }
-};
+// const confirmDelete = (adId) => {
+//     if (confirm(`Are you sure you want to delete ad with ID ${adId}?`)) {
+//         console.log(`Deleting ad with ID ${adId}`);
+//     }
+// };
 </script>
 
 <style scoped>
--- resources/js/Components/Tribekoto/PostItem.vue
+++ resources/js/Components/Tribekoto/PostItem.vue
@@ -1,48 +1,66 @@
 <script setup>
-import { ChatBubbleLeftRightIcon, HandThumbUpIcon } from '@heroicons/vue/24/outline'
-import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
+import {
+    ChatBubbleLeftRightIcon,
+    HandThumbUpIcon,
+} from "@heroicons/vue/24/outline";
+import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
 import PostUserHeader from "@/Components/Tribekoto/PostUserHeader.vue";
-import { router, useForm, usePage } from '@inertiajs/vue3'
+import { router, useForm, usePage } from "@inertiajs/vue3";
 import axiosClient from "@/axiosClient.js";
 import ReadMoreReadLess from "@/Components/Tribekoto/ReadMoreReadLess.vue";
 import EditDeleteDropdown from "@/Components/Tribekoto/EditDeleteDropdown.vue";
 import PostAttachments from "@/Components/Tribekoto/PostAttachments.vue";
 import CommentList from "@/Components/Tribekoto/CommentList.vue";
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import UrlPreview from "@/Components/Tribekoto/UrlPreview.vue";
 import { MapPinIcon } from "@heroicons/vue/24/outline/index.js";
-import Swal from 'sweetalert2';
+import {
+    Dialog,
+    DialogPanel,
+    DialogTitle,
+    TransitionChild,
+    TransitionRoot,
+} from "@headlessui/vue";
+import { ExclamationTriangleIcon } from "@heroicons/vue/24/outline";
+
+const open = ref(false);
+
+const openDeleteModal = () => {
+    open.value = true;
+};
 
 const props = defineProps({
-    post: Object
-})
+    post: Object,
+});
 
 const authUser = usePage().props.auth.user;
 const group = usePage().props.group;
 
-const emit = defineEmits(['editClick', 'attachmentClick'])
+const emit = defineEmits(["editClick", "attachmentClick"]);
 
 const postBody = computed(() => {
     let content = props.post.body.replace(
         /(?:(\s+)|<p>)((#\w+)(?![^<]*<\/a>))/g,
         (match, group1, group2) => {
             const encodedGroup = encodeURIComponent(group2);
-            return `${group1 || ''}<a href="/search/${encodedGroup}" class="hashtag">${group2}</a>`;
+            return `${
+                group1 || ""
+            }<a href="/search/${encodedGroup}" class="hashtag">${group2}</a>`;
         }
-    )
+    );
 
     return content;
-})
+});
 const isPinned = computed(() => {
     if (group?.id) {
-        return group?.pinned_post_id === props.post.id
+        return group?.pinned_post_id === props.post.id;
     }
 
-    return authUser?.pinned_post_id === props.post.id
-})
+    return authUser?.pinned_post_id === props.post.id;
+});
 
 function openEditModal() {
-    emit('editClick', props.post)
+    emit("editClick", props.post);
 }
 
 // function deletePost() {
@@ -52,33 +70,23 @@ function openEditModal() {
 //         })
 //     }
 // }
-const deletePost = () => {
-    Swal.fire({
-        title: 'Are you sure?',
-        text: 'Are you sure you want to delete this post?',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            router.delete(route('post.destroy', props.post), {
-                preserveScroll: true
-            }).then(() => {
-                // Optionally handle success or additional logic after deletion
-            }).catch((error) => {
-                // Handle error if deletion fails
-                console.error('Failed to delete post:', error);
-            });
-        }
-    });
+const confirmDelete = () => {
+    router
+        .delete(route("post.destroy", props.post), {
+            preserveScroll: true,
+        })
+        .then(() => {
+            open.value = false;
+        })
+        .catch((error) => {
+            console.error("Failed to delete post:", error);
+        });
 };
 
 function pinUnpinPost() {
     const form = useForm({
-        forGroup: group?.id
-    })
+        forGroup: group?.id,
+    });
     let isPinned = false;
     if (group?.id) {
         isPinned = group?.pinned_post_id === props.post.id;
@@ -86,36 +94,39 @@ function pinUnpinPost() {
         isPinned = authUser?.pinned_post_id === props.post.id;
     }
 
-    form.post(route('post.pinUnpin', props.post.id), {
+    form.post(route("post.pinUnpin", props.post.id), {
         preserveScroll: true,
         onSuccess: () => {
             if (group?.id) {
-                group.pinned_post_id = isPinned ? null : props.post.id
+                group.pinned_post_id = isPinned ? null : props.post.id;
             } else {
-                authUser.pinned_post_id = isPinned ? null : props.post.id
+                authUser.pinned_post_id = isPinned ? null : props.post.id;
             }
-        }
-    })
+        },
+    });
 }
 
 function openAttachment(ind) {
-    emit('attachmentClick', props.post, ind)
+    emit("attachmentClick", props.post, ind);
 }
 
 function sendReaction() {
-    axiosClient.post(route('post.reaction', props.post), {
-        reaction: 'like'
-    })
-        .then(({ data }) => {
-            props.post.current_user_has_reaction = data.current_user_has_reaction
-            props.post.num_of_reactions = data.num_of_reactions;
+    axiosClient
+        .post(route("post.reaction", props.post), {
+            reaction: "like",
         })
+        .then(({ data }) => {
+            props.post.current_user_has_reaction =
+                data.current_user_has_reaction;
+            props.post.num_of_reactions = data.num_of_reactions;
+        });
 }
-
 </script>
 
 <template>
-    <div class="bg-white border dark:bg-slate-950 dark:border-slate-900 dark:text-gray-100 rounded p-4 mb-3">
+    <div
+        class="bg-white border dark:bg-slate-950 dark:border-slate-900 dark:text-gray-100 rounded p-4 mb-3"
+    >
         <div class="flex items-center justify-between mb-3">
             <PostUserHeader :post="post" />
             <div class="flex items-center gap-2">
@@ -123,45 +134,158 @@ function sendReaction() {
                     <MapPinIcon class="h-3 w-3" aria-hidden="true" />
                     pinned
                 </div>
-                <EditDeleteDropdown :user="post.user" :post="post" @edit="openEditModal" @delete="deletePost"
-                    @pin="pinUnpinPost" />
+                <!-- <EditDeleteDropdown
+                    :user="post.user"
+                    :post="post"
+                    @edit="openEditModal"
+                    @delete="deletePost"
+                    @pin="pinUnpinPost"
+                /> -->
+                <EditDeleteDropdown
+                    :user="post.user"
+                    :post="post"
+                    @edit="openEditModal"
+                    @delete="openDeleteModal"
+                    @pin="pinUnpinPost"
+                />
             </div>
         </div>
         <div class="mb-3">
             <ReadMoreReadLess :content="postBody" />
             <UrlPreview :preview="post.preview" :url="post.preview_url" />
         </div>
-        <div class="grid gap-3 mb-3" :class="[
-            post.attachments.length === 1 ? 'grid-cols-1' : 'grid-cols-2'
-        ]">
-            <PostAttachments :attachments="post.attachments" @attachmentClick="openAttachment" />
+        <div
+            class="grid gap-3 mb-3"
+            :class="[
+                post.attachments.length === 1 ? 'grid-cols-1' : 'grid-cols-2',
+            ]"
+        >
+            <PostAttachments
+                :attachments="post.attachments"
+                @attachmentClick="openAttachment"
+            />
         </div>
         <Disclosure v-slot="{ open }">
             <!--            Like & Comment buttons-->
             <div class="flex gap-2">
-                <button @click="sendReaction"
-                    class="text-gray-800 dark:text-gray-100 flex gap-1 items-center justify-center  rounded-lg py-2 px-4 flex-1"
+                <button
+                    @click="sendReaction"
+                    class="text-gray-800 dark:text-gray-100 flex gap-1 items-center justify-center rounded-lg py-2 px-4 flex-1"
                     :class="[
-                        post.current_user_has_reaction ?
-                            'bg-red-300 dark:bg-sky-900 hover:bg-red-400 dark:hover:bg-sky-950' :
-                            'bg-red-100 dark:bg-slate-900 hover:bg-red-200 dark:hover:bg-slate-800'
-                    ]">
+                        post.current_user_has_reaction
+                            ? 'bg-red-300 dark:bg-sky-900 hover:bg-red-400 dark:hover:bg-sky-950'
+                            : 'bg-red-100 dark:bg-slate-900 hover:bg-red-200 dark:hover:bg-slate-800',
+                    ]"
+                >
                     <HandThumbUpIcon class="w-5 h-5" />
                     <span class="mr-2">{{ post.num_of_reactions }}</span>
-                    {{ post.current_user_has_reaction ? 'Unlike' : 'Like' }}
+                    {{ post.current_user_has_reaction ? "Unlike" : "Like" }}
                 </button>
                 <DisclosureButton
-                    class="text-gray-800 dark:text-gray-100 flex gap-1 items-center justify-center bg-gray-100 dark:bg-slate-900 dark:hover:bg-slate-800 rounded-lg hover:bg-gray-200  py-2 px-4 flex-1">
+                    class="text-gray-800 dark:text-gray-100 flex gap-1 items-center justify-center bg-gray-100 dark:bg-slate-900 dark:hover:bg-slate-800 rounded-lg hover:bg-gray-200 py-2 px-4 flex-1"
+                >
                     <ChatBubbleLeftRightIcon class="w-5 h-5" />
                     <span class="mr-2">{{ post.num_of_comments }}</span>
                     Comment
                 </DisclosureButton>
             </div>
 
-            <DisclosurePanel class="comment-list mt-3 max-h-[400px] overflow-auto">
+            <DisclosurePanel
+                class="comment-list mt-3 max-h-[400px] overflow-auto"
+            >
                 <CommentList :post="post" :data="{ comments: post.comments }" />
             </DisclosurePanel>
         </Disclosure>
 
+        <!-- Delete Modal -->
+        <TransitionRoot as="template" :show="open">
+            <Dialog class="relative z-10" @close="open = false">
+                <TransitionChild
+                    as="template"
+                    enter="ease-out duration-300"
+                    enter-from="opacity-0"
+                    enter-to="opacity-100"
+                    leave="ease-in duration-200"
+                    leave-from="opacity-100"
+                    leave-to="opacity-0"
+                >
+                    <div
+                        class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+                    />
+                </TransitionChild>
+
+                <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+                    <div
+                        class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0"
+                    >
+                        <TransitionChild
+                            as="template"
+                            enter="ease-out duration-300"
+                            enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                            enter-to="opacity-100 translate-y-0 sm:scale-100"
+                            leave="ease-in duration-200"
+                            leave-from="opacity-100 translate-y-0 sm:scale-100"
+                            leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        >
+                            <DialogPanel
+                                class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg"
+                            >
+                                <div
+                                    class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4"
+                                >
+                                    <div class="sm:flex sm:items-start">
+                                        <div
+                                            class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10"
+                                        >
+                                            <ExclamationTriangleIcon
+                                                class="h-6 w-6 text-red-600"
+                                                aria-hidden="true"
+                                            />
+                                        </div>
+                                        <div
+                                            class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left"
+                                        >
+                                            <DialogTitle
+                                                as="h3"
+                                                class="text-base font-semibold leading-6 text-gray-900"
+                                            >
+                                                Delete Post
+                                            </DialogTitle>
+                                            <div class="mt-2">
+                                                <p
+                                                    class="text-sm text-gray-500"
+                                                >
+                                                    Are you sure you want to
+                                                    delete this post?
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div
+                                    class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6"
+                                >
+                                    <button
+                                        type="button"
+                                        class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                                        @click="confirmDelete"
+                                    >
+                                        Delete
+                                    </button>
+                                    <button
+                                        type="button"
+                                        class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                                        @click="open = false"
+                                        ref="cancelButtonRef"
+                                    >
+                                        Cancel
+                                    </button>
+                                </div>
+                            </DialogPanel>
+                        </TransitionChild>
+                    </div>
+                </div>
+            </Dialog>
+        </TransitionRoot>
     </div>
 </template>
