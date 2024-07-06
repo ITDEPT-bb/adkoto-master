768bbb8896a9550ae93b559dcdc1c97fecf719a2
TianTianny <acdeocera.bb88@gmail.com>
2024-07-06 13:50:33 +0800

    fiximg chat layout logo

--- resources/js/Layouts/ChatLayout.vue
+++ resources/js/Layouts/ChatLayout.vue
@@ -51,7 +51,7 @@ function toggleDarkMode() {
                         <!-- Logo -->
                         <div class="shrink-0 flex items-center">
                             <Link :href="route('chat.index')">
-                            <ApplicationLogo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                            <!-- <ApplicationLogo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" /> -->
                             <img :src="logo" class="block h-9 w-auto" alt="Logo" />
                         </Link>
                         </div>
