c2124c803db0f59513a9d30c9bbf7c2970a4f024
TianTianny <acdeocera.bb88@gmail.com>
2024-07-23 14:13:48 +0800

    type message style

--- resources/js/Components/Chat/MessageInput.vue
+++ resources/js/Components/Chat/MessageInput.vue
@@ -1,50 +1,52 @@
 <template>
     <div class="border-t-2 border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
-        <div class="relative flex">
-            <span class="absolute inset-y-0 flex items-center">
-                <button
-                    type="button"
-                    class="inline-flex items-center justify-center rounded-full h-12 w-12 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none"
-                >
-                    <MicIcon />
-                </button>
-            </span>
-            <input
+        <div
+            class="flex items-center px-3 py-2 rounded-lg bg-gray-50 dark:bg-gray-700"
+        >
+            <button
+                type="button"
+                class="inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
+            >
+                <MicIcon />
+                <span class="sr-only">Record audio</span>
+            </button>
+            <button
+                type="button"
+                class="p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
+            >
+                <PaperClipIcon />
+                <span class="sr-only">Attach file</span>
+            </button>
+            <button
+                type="button"
+                class="p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
+            >
+                <CameraIcon />
+                <span class="sr-only">Upload image</span>
+            </button>
+            <button
+                type="button"
+                class="p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
+            >
+                <EmojiIcon />
+                <span class="sr-only">Add emoji</span>
+            </button>
+            <textarea
                 v-model="newMessage"
                 @keyup.enter="sendMessage"
-                type="text"
-                placeholder="Write your message!"
-                class="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-12 bg-gray-200 rounded-md py-3 resize-none"
-                rows="4"
-            />
-            <div class="absolute right-0 items-center inset-y-0 hidden sm:flex">
-                <button
-                    type="button"
-                    class="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none"
-                >
-                    <PaperClipIcon />
-                </button>
-                <button
-                    type="button"
-                    class="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none"
-                >
-                    <CameraIcon />
-                </button>
-                <button
-                    type="button"
-                    class="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none"
-                >
-                    <EmojiIcon />
-                </button>
-                <button
-                    @click="sendMessage"
-                    type="button"
-                    class="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none"
-                >
-                    <span class="font-bold">Send</span>
-                    <SendIcon />
-                </button>
-            </div>
+                id="chat"
+                rows="1"
+                class="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Write your message..."
+            ></textarea>
+            <button
+                @click="sendMessage"
+                type="button"
+                class="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600"
+            >
+                <SendIcon />
+                <span class="sr-only">Send message</span>
+            </button>
         </div>
     </div>
 </template>
