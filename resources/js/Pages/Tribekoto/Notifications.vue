e4a47e26aff4eccb3e09582be9c1c7c7428632ca
TianTianny <acdeocera.bb88@gmail.com>
2024-07-31 17:06:48 +0800

    scrollbar

--- resources/js/Pages/Tribekoto/Notifications.vue
+++ resources/js/Pages/Tribekoto/Notifications.vue
@@ -5,7 +5,7 @@
         <div class="bg-gray-100 py-4 px-8">
             <h1 class="text-2xl font-bold text-gray-800">All Notifications</h1>
         </div>
-        <div class="container mx-auto p-4 py-2">
+        <div class="container mx-auto p-4 py-2 h-full overflow-y-auto">
             <template v-if="notifications.length === 0">
                 <div class="p-4 text-center text-gray-500">
                     No notifications available.
