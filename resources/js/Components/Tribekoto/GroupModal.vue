2391f584ebfb291f7b1dd3ce743e02681368ac26
TianTianny <acdeocera.bb88@gmail.com>
2024-08-02 13:43:00 +0800

    group disabled

--- resources/js/Components/Tribekoto/GroupModal.vue
+++ resources/js/Components/Tribekoto/GroupModal.vue
@@ -1,13 +1,13 @@
 <script setup>
-import { computed, ref } from 'vue'
-import { XMarkIcon, BookmarkIcon } from '@heroicons/vue/24/solid'
+import { computed, ref } from "vue";
+import { XMarkIcon, BookmarkIcon } from "@heroicons/vue/24/solid";
 import {
     TransitionRoot,
     TransitionChild,
     Dialog,
     DialogPanel,
     DialogTitle,
-} from '@headlessui/vue'
+} from "@headlessui/vue";
 import { useForm } from "@inertiajs/vue3";
 import TextInput from "@/Components/TextInput.vue";
 import Checkbox from "@/Components/Checkbox.vue";
@@ -17,43 +17,52 @@ import GroupForm from "@/Components/Tribekoto/GroupForm.vue";
 import BaseModal from "@/Components/Tribekoto/BaseModal.vue";
 
 const props = defineProps({
-    modelValue: Boolean
-})
+    modelValue: Boolean,
+});
 
 const formErrors = ref({});
 
 const form = useForm({
-    name: '',
+    name: "",
     auto_approval: true,
-    about: '',
-})
+    about: "",
+});
 
 const show = computed({
     get: () => props.modelValue,
-    set: (value) => emit('update:modelValue', value)
-})
+    set: (value) => emit("update:modelValue", value),
+});
 
-const emit = defineEmits(['update:modelValue', 'hide', 'create'])
+const emit = defineEmits(["update:modelValue", "hide", "create"]);
 
 function closeModal() {
-    show.value = false
-    emit('hide')
+    show.value = false;
+    emit("hide");
     resetModal();
 }
 
 function resetModal() {
-    form.reset()
-    formErrors.value = {}
+    form.reset();
+    formErrors.value = {};
 }
 
+const isLoading = ref(false);
 function submit() {
-    axiosClient.post(route('group.create'), form)
-        // .then(res => {
-        //     console.log(res)
+    if (isLoading.value) return;
+    isLoading.value = true;
+
+    axiosClient
+        .post(route("group.create"), form)
         .then(({ data }) => {
-            closeModal()
-            emit('create', data)
+            closeModal();
+            emit("create", data);
         })
+        .catch((error) => {
+            console.error(error);
+        })
+        .finally(() => {
+            isLoading.value = false;
+        });
 }
 </script>
 
@@ -64,16 +73,33 @@ function submit() {
         </div>
 
         <div class="flex justify-end gap-2 py-3 px-4">
-            <button @click="show = false"
-                class="text-gray-800 flex gap-1 items-center justify-center bg-gray-100 rounded-md hover:bg-gray-200 py-2 px-4">
+            <button
+                @click="show = false"
+                class="text-gray-800 flex gap-1 items-center justify-center bg-gray-100 rounded-md hover:bg-gray-200 py-2 px-4"
+            >
                 <XMarkIcon class="w-5 h-5" />
                 Cancel
             </button>
-            <button type="button"
+            <!-- <button type="button"
                 class="flex items-center justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
                 @click="submit">
                 <BookmarkIcon class="w-4 h-4 mr-2" />
                 Submit
+            </button> -->
+            <button
+                type="button"
+                @click="submit"
+                :class="[
+                    isLoading
+                        ? 'bg-gray-300 cursor-not-allowed'
+                        : 'bg-red-600 hover:bg-red-500',
+                    'flex items-center justify-center rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600',
+                ]"
+                :disabled="isLoading"
+            >
+                <BookmarkIcon class="w-4 h-4 mr-2" />
+                <span v-if="!isLoading">Submit</span>
+                <span v-else>Loading...</span>
             </button>
         </div>
     </BaseModal>
