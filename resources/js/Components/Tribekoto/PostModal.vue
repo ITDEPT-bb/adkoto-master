4d530235740bc8d357250c0d8f40cc5339c8eb11
TianTianny <acdeocera.bb88@gmail.com>
2024-08-02 11:44:42 +0800

    disabled button when submitting post

--- resources/js/Components/Tribekoto/PostModal.vue
+++ resources/js/Components/Tribekoto/PostModal.vue
@@ -1,6 +1,11 @@
 <script setup>
-import { computed, ref, watch } from 'vue'
-import { XMarkIcon, PaperClipIcon, BookmarkIcon, ArrowUturnLeftIcon } from '@heroicons/vue/24/solid'
+import { computed, ref, watch } from "vue";
+import {
+    XMarkIcon,
+    PaperClipIcon,
+    BookmarkIcon,
+    ArrowUturnLeftIcon,
+} from "@heroicons/vue/24/solid";
 import PostUserHeader from "@/Components/Tribekoto/PostUserHeader.vue";
 import { useForm, usePage } from "@inertiajs/vue3";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
@@ -13,22 +18,47 @@ import BaseModal from "@/Components/Tribekoto/BaseModal.vue";
 const editor = ClassicEditor;
 const editorConfig = {
     mediaEmbed: {
-        removeProviders: ['dailymotion', 'spotify', 'youtube', 'vimeo', 'instagram', 'twitter', 'googleMaps', 'flickr', 'facebook']
+        removeProviders: [
+            "dailymotion",
+            "spotify",
+            "youtube",
+            "vimeo",
+            "instagram",
+            "twitter",
+            "googleMaps",
+            "flickr",
+            "facebook",
+        ],
     },
-    toolbar: ['bold', 'italic', '|', 'bulletedList', 'numberedList', '|', 'heading', '|', 'outdent', 'indent', '|', 'link', '|', 'blockQuote'],
-}
+    toolbar: [
+        "bold",
+        "italic",
+        "|",
+        "bulletedList",
+        "numberedList",
+        "|",
+        "heading",
+        "|",
+        "outdent",
+        "indent",
+        "|",
+        "link",
+        "|",
+        "blockQuote",
+    ],
+};
 
 const props = defineProps({
     post: {
         type: Object,
-        required: true
+        required: true,
     },
     group: {
         type: Object,
-        default: null
+        default: null,
     },
-    modelValue: Boolean
-})
+    modelValue: Boolean,
+});
 
 const attachmentExtensions = usePage().props.attachmentExtensions;
 /**
@@ -38,100 +68,109 @@ const attachmentExtensions = usePage().props.attachmentExtensions;
  * }
  * @type {Ref<UnwrapRef<*[]>>}
  */
-const attachmentFiles = ref([])
-const attachmentErrors = ref([])
+const attachmentFiles = ref([]);
+const isLoading = ref(false);
+const attachmentErrors = ref([]);
 const formErrors = ref({});
-const aiButtonLoading = ref(false);
 
 const form = useForm({
-    body: '',
+    body: "",
     group_id: null,
     attachments: [],
     deleted_file_ids: [],
     preview: {},
     preview_url: null,
-    _method: 'POST'
-})
+    _method: "POST",
+});
 
 const show = computed({
     get: () => props.modelValue,
-    set: (value) => emit('update:modelValue', value)
-})
+    set: (value) => emit("update:modelValue", value),
+});
 
 const computedAttachments = computed(() => {
-    return [...attachmentFiles.value, ...(props.post.attachments || [])]
-})
+    return [...attachmentFiles.value, ...(props.post.attachments || [])];
+});
 const showExtensionsText = computed(() => {
     for (let myFile of attachmentFiles.value) {
-        const file = myFile.file
-        let parts = file.name.split('.')
-        let ext = parts.pop().toLowerCase()
+        const file = myFile.file;
+        let parts = file.name.split(".");
+        let ext = parts.pop().toLowerCase();
         if (!attachmentExtensions.includes(ext)) {
-            return true
+            return true;
         }
     }
 
     return false;
-})
+});
 
-const emit = defineEmits(['update:modelValue', 'hide'])
+const emit = defineEmits(["update:modelValue", "hide"]);
 
-watch(() => props.post, () => {
-    form.body = props.post.body || ''
-    onInputChange();
-})
+watch(
+    () => props.post,
+    () => {
+        form.body = props.post.body || "";
+        onInputChange();
+    }
+);
 
 function closeModal() {
-    show.value = false
-    emit('hide')
+    show.value = false;
+    emit("hide");
     resetModal();
 }
 
 function resetModal() {
-    form.reset()
-    formErrors.value = {}
-    attachmentFiles.value = []
+    form.reset();
+    formErrors.value = {};
+    attachmentFiles.value = [];
     attachmentErrors.value = [];
     if (props.post.attachments) {
-        props.post.attachments.forEach(file => file.deleted = false)
+        props.post.attachments.forEach((file) => (file.deleted = false));
     }
 }
 
 function submit() {
+    isLoading.value = true;
+
     if (props.group) {
-        form.group_id = props.group.id
+        form.group_id = props.group.id;
     }
-    form.attachments = attachmentFiles.value.map(myFile => myFile.file)
+    form.attachments = attachmentFiles.value.map((myFile) => myFile.file);
     if (props.post.id) {
-        form._method = 'PUT'
-        form.post(route('post.update', props.post.id), {
+        form._method = "PUT";
+        form.post(route("post.update", props.post.id), {
             preserveScroll: true,
             onSuccess: (res) => {
-                closeModal()
+                isLoading.value = false;
+                closeModal();
             },
             onError: (errors) => {
-                processErrors(errors)
-            }
-        })
+                isLoading.value = false;
+                processErrors(errors);
+            },
+        });
     } else {
-        form.post(route('post.create'), {
+        form.post(route("post.create"), {
             preserveScroll: true,
             onSuccess: (res) => {
-                closeModal()
+                isLoading.value = false;
+                closeModal();
             },
             onError: (errors) => {
-                processErrors(errors)
-            }
-        })
+                isLoading.value = false;
+                processErrors(errors);
+            },
+        });
     }
 }
 
 function processErrors(errors) {
-    formErrors.value = errors
+    formErrors.value = errors;
     for (const key in errors) {
-        if (key.includes('.')) {
-            const [, index] = key.split('.')
-            attachmentErrors.value[index] = errors[key]
+        if (key.includes(".")) {
+            const [, index] = key.split(".");
+            attachmentErrors.value[index] = errors[key];
         }
     }
 }
@@ -140,9 +179,9 @@ async function onAttachmentChoose($event) {
     for (const file of $event.target.files) {
         const myFile = {
             file,
-            url: await readFile(file)
-        }
-        attachmentFiles.value.push(myFile)
+            url: await readFile(file),
+        };
+        attachmentFiles.value.push(myFile);
     }
     $event.target.value = null;
 }
@@ -152,46 +191,32 @@ async function readFile(file) {
         if (isImage(file)) {
             const reader = new FileReader();
             reader.onload = () => {
-                res(reader.result)
-            }
-            reader.onerror = rej
-            reader.readAsDataURL(file)
+                res(reader.result);
+            };
+            reader.onerror = rej;
+            reader.readAsDataURL(file);
         } else {
-            res(null)
+            res(null);
         }
-    })
+    });
 }
 
 function removeFile(myFile) {
     if (myFile.file) {
-        attachmentFiles.value = attachmentFiles.value.filter(f => f !== myFile)
+        attachmentFiles.value = attachmentFiles.value.filter(
+            (f) => f !== myFile
+        );
     } else {
-        form.deleted_file_ids.push(myFile.id)
-        myFile.deleted = true
+        form.deleted_file_ids.push(myFile.id);
+        myFile.deleted = true;
     }
 }
 
 function undoDelete(myFile) {
     myFile.deleted = false;
-    form.deleted_file_ids = form.deleted_file_ids.filter(id => myFile.id !== id)
-}
-
-function getAIContent() {
-    if (!form.body) {
-        return;
-    }
-    aiButtonLoading.value = true;
-    axiosClient.post(route('post.aiContent'), {
-        prompt: form.body
-    })
-        .then(({ data }) => {
-            form.body = data.content
-            aiButtonLoading.value = false;
-        })
-        .catch(err => {
-            console.log(err)
-            aiButtonLoading.value = false;
-        })
+    form.deleted_file_ids = form.deleted_file_ids.filter(
+        (id) => myFile.id !== id
+    );
 }
 
 function fetchPreview(url) {
@@ -199,31 +224,31 @@ function fetchPreview(url) {
         return;
     }
 
-    form.preview_url = url
-    form.preview = {}
+    form.preview_url = url;
+    form.preview = {};
     if (url) {
-        axiosClient.post(route('post.fetchUrlPreview'), { url })
+        axiosClient
+            .post(route("post.fetchUrlPreview"), { url })
             .then(({ data }) => {
                 form.preview = {
-                    title: data['og:title'],
-                    description: data['og:description'],
-                    image: data['og:image']
-                }
+                    title: data["og:title"],
+                    description: data["og:description"],
+                    image: data["og:image"],
+                };
             })
-            .catch(err => {
-                console.log(err)
-            })
+            .catch((err) => {
+                console.log(err);
+            });
     }
 }
 
-
 function onInputChange() {
-    let url = matchHref()
+    let url = matchHref();
 
     if (!url) {
-        url = matchLink()
+        url = matchLink();
     }
-    fetchPreview(url)
+    fetchPreview(url);
 }
 
 function matchHref() {
@@ -251,22 +276,37 @@ function matchLink() {
     if (match && match.length > 0) {
         return match[0];
     }
-    return null
+    return null;
 }
-
 </script>
 
 <template>
-    <BaseModal :title="post.id ? 'Update Post' : 'Create Post'" v-model="show" @hide="closeModal">
+    <BaseModal
+        :title="post.id ? 'Update Post' : 'Create Post'"
+        v-model="show"
+        @hide="closeModal"
+    >
         <div class="p-4">
-            <PostUserHeader :post="post" :show-time="false" class="mb-4 dark:text-gray-100" />
+            <PostUserHeader
+                :post="post"
+                :show-time="false"
+                class="mb-4 dark:text-gray-100"
+            />
 
-            <div v-if="formErrors.group_id" class="bg-red-400 py-2 px-3 rounded text-white mb-3">
+            <div
+                v-if="formErrors.group_id"
+                class="bg-red-400 py-2 px-3 rounded text-white mb-3"
+            >
                 {{ formErrors.group_id }}
             </div>
 
             <div class="relative group">
-                <ckeditor :editor="editor" v-model="form.body" :config="editorConfig" @input="onInputChange"></ckeditor>
+                <ckeditor
+                    :editor="editor"
+                    v-model="form.body"
+                    :config="editorConfig"
+                    @input="onInputChange"
+                ></ckeditor>
 
                 <UrlPreview :preview="form.preview" :url="form.preview_url" />
 
@@ -290,40 +330,64 @@ function matchLink() {
                 </button> -->
             </div>
 
-            <div v-if="showExtensionsText"
-                class="border-l-4 border-amber-500 py-2 px-3 bg-amber-100 mt-3 text-gray-800">
-                Files must be one of the following extensions <br>
-                <small>{{ attachmentExtensions.join(', ') }}</small>
+            <div
+                v-if="showExtensionsText"
+                class="border-l-4 border-amber-500 py-2 px-3 bg-amber-100 mt-3 text-gray-800"
+            >
+                Files must be one of the following extensions <br />
+                <small>{{ attachmentExtensions.join(", ") }}</small>
             </div>
 
-            <div v-if="formErrors.attachments"
-                class="border-l-4 border-red-500 py-2 px-3 bg-red-100 mt-3 text-gray-800">
+            <div
+                v-if="formErrors.attachments"
+                class="border-l-4 border-red-500 py-2 px-3 bg-red-100 mt-3 text-gray-800"
+            >
                 {{ formErrors.attachments }}
             </div>
 
-            <div class="grid gap-3 my-3" :class="[
-                computedAttachments.length === 1 ? 'grid-cols-1' : 'grid-cols-2'
-            ]">
+            <div
+                class="grid gap-3 my-3"
+                :class="[
+                    computedAttachments.length === 1
+                        ? 'grid-cols-1'
+                        : 'grid-cols-2',
+                ]"
+            >
                 <div v-for="(myFile, ind) of computedAttachments">
-                    <div class="group aspect-square bg-blue-100 flex flex-col items-center justify-center text-gray-500 relative border-2"
-                        :class="attachmentErrors[ind] ? 'border-red-500' : ''">
-
-                        <div v-if="myFile.deleted"
-                            class="absolute z-10 left-0 bottom-0 right-0 py-2 px-3 text-sm bg-black text-white flex justify-between items-center">
+                    <div
+                        class="group aspect-square bg-blue-100 flex flex-col items-center justify-center text-gray-500 relative border-2"
+                        :class="attachmentErrors[ind] ? 'border-red-500' : ''"
+                    >
+                        <div
+                            v-if="myFile.deleted"
+                            class="absolute z-10 left-0 bottom-0 right-0 py-2 px-3 text-sm bg-black text-white flex justify-between items-center"
+                        >
                             To be deleted
 
-                            <ArrowUturnLeftIcon @click="undoDelete(myFile)" class="w-4 h-4 cursor-pointer" />
+                            <ArrowUturnLeftIcon
+                                @click="undoDelete(myFile)"
+                                class="w-4 h-4 cursor-pointer"
+                            />
                         </div>
 
-                        <button @click="removeFile(myFile)"
-                            class="absolute z-20 right-3 top-3 w-7 h-7 flex items-center justify-center bg-black/30 text-white rounded-full hover:bg-black/40">
+                        <button
+                            @click="removeFile(myFile)"
+                            class="absolute z-20 right-3 top-3 w-7 h-7 flex items-center justify-center bg-black/30 text-white rounded-full hover:bg-black/40"
+                        >
                             <XMarkIcon class="h-5 w-5" />
                         </button>
 
-                        <img v-if="isImage(myFile.file || myFile)" :src="myFile.url"
-                            class="object-contain aspect-square" :class="myFile.deleted ? 'opacity-50' : ''" />
-                        <div v-else class="flex flex-col justify-center items-center px-3"
-                            :class="myFile.deleted ? 'opacity-50' : ''">
+                        <img
+                            v-if="isImage(myFile.file || myFile)"
+                            :src="myFile.url"
+                            class="object-contain aspect-square"
+                            :class="myFile.deleted ? 'opacity-50' : ''"
+                        />
+                        <div
+                            v-else
+                            class="flex flex-col justify-center items-center px-3"
+                            :class="myFile.deleted ? 'opacity-50' : ''"
+                        >
                             <PaperClipIcon class="w-10 h-10 mb-3" />
 
                             <small class="text-center">
@@ -331,25 +395,53 @@ function matchLink() {
                             </small>
                         </div>
                     </div>
-                    <small class="text-red-500">{{ attachmentErrors[ind] }}</small>
+                    <small class="text-red-500">{{
+                        attachmentErrors[ind]
+                    }}</small>
                 </div>
             </div>
-
         </div>
 
         <div class="flex gap-2 py-3 px-4">
-            <button type="button"
-                class="flex items-center justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 w-full relative">
+            <button
+                type="button"
+                class="flex items-center justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 w-full relative"
+            >
                 <PaperClipIcon class="w-4 h-4 mr-2" />
                 Attach Files
-                <input @click.stop @change="onAttachmentChoose" type="file" multiple
-                    class="absolute left-0 top-0 right-0 bottom-0 opacity-0">
+                <input
+                    @click.stop
+                    @change="onAttachmentChoose"
+                    type="file"
+                    multiple
+                    class="absolute left-0 top-0 right-0 bottom-0 opacity-0"
+                />
             </button>
-            <button type="button"
+            <!-- <button
+                type="button"
                 class="flex items-center justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 w-full"
-                @click="submit">
+                @click="submit"
+            >
                 <BookmarkIcon class="w-4 h-4 mr-2" />
                 Submit
+            </button> -->
+            <button
+                type="button"
+                :class="[
+                    'flex items-center justify-center rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm w-full',
+                    isLoading
+                        ? 'bg-gray-300 cursor-not-allowed'
+                        : 'bg-red-600 hover:bg-red-500',
+                    isLoading
+                        ? ''
+                        : 'focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600',
+                ]"
+                :disabled="isLoading"
+                @click="submit"
+            >
+                <BookmarkIcon class="w-4 h-4 mr-2" />
+                <span v-if="!isLoading">Submit</span>
+                <span v-else>Loading...</span>
             </button>
         </div>
     </BaseModal>
