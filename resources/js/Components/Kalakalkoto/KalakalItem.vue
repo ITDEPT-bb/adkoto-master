bbed41c780ed220a01364cf1a4fc8710e4db1da8
TianTianny <acdeocera.bb88@gmail.com>
2024-08-22 16:49:55 +0800

    fixing kalakalkoto item cards

--- resources/js/Components/Kalakalkoto/KalakalItem.vue
+++ resources/js/Components/Kalakalkoto/KalakalItem.vue
@@ -20,25 +20,44 @@
                 />
             </div>
 
-            <div class="p-4 flex flex-col items-start space-y-2">
-                <div class="flex flex-col justify-between w-full">
+            <div class="p-2 flex flex-col space-y-4">
+                <div class="flex flex-col w-full space-y-2">
                     <h2
-                        class="text-lg font-semibold text-gray-800 dark:text-gray-200"
+                        class="text-xl font-semibold text-gray-900 dark:text-gray-100 line-clamp-1"
                     >
-                        {{ item.title }}
+                        {{ item.name }}
                     </h2>
                     <p
-                        class="text-lg font-bold text-gray-900 dark:text-gray-100"
+                        class="text-lg font-bold text-gray-900 dark:text-gray-200"
                     >
                         {{ formatPrice(item.price) }}
                     </p>
-                    <p class="text-md text-gray-900 dark:text-gray-100">
+                    <p
+                        class="text-base text-gray-700 dark:text-gray-300 line-clamp-1"
+                    >
                         {{ item.description }}
                     </p>
                 </div>
-                <p class="text-sm text-gray-600 dark:text-gray-400">
-                    Location: {{ item.location }}
-                </p>
+                <div class="flex items-center text-gray-700 dark:text-gray-300">
+                    <MapPinIcon
+                        class="w-5 h-5 mr-2 text-gray-500 dark:text-gray-400"
+                    />
+                    <p
+                        class="text-sm font-semibold text-gray-600 dark:text-gray-400"
+                    >
+                        {{ item.location }}
+                    </p>
+                </div>
+                <div class="flex items-center text-gray-700 dark:text-gray-300">
+                    <UserIcon
+                        class="w-5 h-5 mr-2 text-gray-500 dark:text-gray-400"
+                    />
+                    <p
+                        class="text-sm font-semibold text-gray-600 dark:text-gray-400"
+                    >
+                        {{ item.user.name }}
+                    </p>
+                </div>
             </div>
         </div>
     </a>
@@ -46,6 +65,8 @@
 
 <script setup>
 import { defineProps } from "vue";
+import UserIcon from "@/Components/Icons/UserIcon.vue";
+import MapPinIcon from "@/Components/Icons/MapPinIcon.vue";
 
 const props = defineProps({
     item: Object,
