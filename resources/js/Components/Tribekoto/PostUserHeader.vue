8137477e65a686968b0ec2fbe98e8dd419789572
TianTianny <acdeocera.bb88@gmail.com>
2024-10-07 14:56:40 +0800

    post time when created

--- resources/js/Components/Tribekoto/PostUserHeader.vue
+++ resources/js/Components/Tribekoto/PostUserHeader.vue
@@ -1,43 +1,63 @@
 <script setup>
-import { Link } from '@inertiajs/vue3';
+import { Link } from "@inertiajs/vue3";
 import { ChevronRightIcon } from "@heroicons/vue/24/solid/index.js";
-
-import dayjs from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime';
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
 dayjs.extend(relativeTime);
 
 defineProps({
-    post: {
-        type: Object
-    },
-    showTime: {
-        type: Boolean,
-        default: true
-    }
-})
+	post: {
+		type: Object,
+	},
+	showTime: {
+		type: Boolean,
+		default: true,
+	},
+});
+
+function formatTime(postDate) {
+	const now = dayjs();
+	const postTime = dayjs(postDate);
+
+	if (now.diff(postTime, "week") >= 1) {
+		return postTime.format("MMMM D, YYYY");
+	} else {
+		return postTime.fromNow();
+	}
+}
 </script>
 
 <template>
-    <div class="flex items-center gap-2 ">
-        <Link :href="route('profile', post.user.username)">
-        <img :src="post.user.avatar_url" class="w-10 h-10 object-cover rounded-full border-2 transition-all hover:border-red-500" />
-        </Link>
-        <div>
-            <h4 class="flex items-center font-bold">
-                <Link :href="route('profile', post.user.username)" class="hover:underline">
-                {{ post.user.name }} {{ post.user.surname }}
-                </Link>
-                <template v-if="post.group">
-                    <ChevronRightIcon class="w-4" />
-                    <Link :href="route('group.profile', post.group.slug)" class="hover:underline">
-                    {{ post.group.name }}
-                    </Link>
-                </template>
-            </h4>
-            <small v-if="showTime" class="text-gray-400">{{ dayjs(post.updated_at).fromNow() }}</small>
-        </div>
-    </div>
+	<div class="flex items-center gap-2">
+		<Link :href="route('profile', post.user.username)">
+			<img
+				:src="post.user.avatar_url"
+				class="w-10 h-10 object-cover rounded-full border-2 transition-all hover:border-red-500" />
+		</Link>
+		<div>
+			<h4 class="flex items-center font-bold">
+				<Link
+					:href="route('profile', post.user.username)"
+					class="hover:underline">
+					{{ post.user.name }} {{ post.user.surname }}
+				</Link>
+				<template v-if="post.group">
+					<ChevronRightIcon class="w-4" />
+					<Link
+						:href="route('group.profile', post.group.slug)"
+						class="hover:underline">
+						{{ post.group.name }}
+					</Link>
+				</template>
+			</h4>
+			<small
+				v-if="showTime"
+				class="text-gray-400"
+				>{{ formatTime(post.created_at) }}</small
+			>
+		</div>
+	</div>
 </template>
 
 <style scoped></style>
