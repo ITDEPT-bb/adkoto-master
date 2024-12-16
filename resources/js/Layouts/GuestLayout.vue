39c493e60ca524bcb7b97ed0916572f3054d80e0
TianTianny <acdeocera.bb88@gmail.com>
2024-12-16 16:55:26 +0800

    logo hotfix on auth page

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,20 +1,20 @@
 <script setup>
-import AdkotoLogo from '@/Components/AdkotoLogo.vue';
-import { Link } from '@inertiajs/vue3';
+import AdkotoLogo from "@/Components/AdkotoLogo.vue";
+import { Link } from "@inertiajs/vue3";
 </script>
 
 <template>
-    <div class="h-full overflow-y-auto flex flex-col sm:mt-0 sm:justify-center mt-24 items-center sm:pt-0 bg-gray-100 dark:bg-gray-900">
-        <div>
-            <Link href="/">
-                <AdkotoLogo class="w-auto h-20 fill-current text-gray-500" />
-            </Link>
-        </div>
+	<div
+		class="h-full overflow-y-auto flex flex-col sm:mt-0 sm:justify-center mt-24 items-center sm:pt-0 bg-gray-100 dark:bg-gray-900">
+		<div>
+			<Link href="/">
+				<AdkotoLogo class="w-auto h-24 fill-current text-gray-500" />
+			</Link>
+		</div>
 
-        <div
-            class="w-full overflow-y-auto sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg"
-        >
-            <slot />
-        </div>
-    </div>
+		<div
+			class="w-full overflow-y-auto sm:max-w-md mt-2 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+			<slot />
+		</div>
+	</div>
 </template>
