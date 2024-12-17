a289e45d286a4fa97356ab400e11bde33c09f901
TianTianny <acdeocera.bb88@gmail.com>
2024-12-17 11:16:18 +0800

    password reset page logo hotfix

---
+++ resources/js/Layouts/ResetPasswordLayout.vue
@@ -0,0 +1,26 @@
+<script setup>
+// import AdkotoLogo from "@/Components/AdkotoLogo.vue";
+import { Link } from "@inertiajs/vue3";
+
+import logo from "/public/img/1aaadkotoa1.png";
+</script>
+
+<template>
+	<div
+		class="h-full overflow-y-auto flex flex-col sm:mt-0 sm:justify-center mt-24 items-center sm:pt-0 bg-gray-100 dark:bg-gray-900">
+		<div>
+			<Link href="/">
+				<!-- <logo class="w-auto h-24 fill-current text-gray-500" /> -->
+				<img
+					:src="logo"
+					class="w-auto h-24 fill-current text-gray-500"
+					alt="" />
+			</Link>
+		</div>
+
+		<div
+			class="w-full overflow-y-auto sm:max-w-md mt-2 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+			<slot />
+		</div>
+	</div>
+</template>
