1d45916db3d37fff4b1d317f6eca2300a8c186a0
Ahllaine Christian De Ocera <acdeocera.bb88@gmail.com>
2025-04-24 15:50:43 +0800

    adjust layout tribekoto

--- resources/js/Components/Tribekoto/GroupItem.vue
+++ resources/js/Components/Tribekoto/GroupItem.vue
@@ -1,24 +1,36 @@
 <script setup>
-import { Link } from '@inertiajs/vue3'
+import { Link } from "@inertiajs/vue3";
 
 defineProps({
     group: Object,
-})
+});
 </script>
 
 <template>
-    <div class="cursor-pointer hover:bg-gray-100 dark:hover:bg-slate-800">
-        <Link :href="route('group.profile', group.slug)" class="flex items-start gap-1 py-2 px-2">
-        <img :src="group.thumbnail_url" class="w-[32px] rounded-full" />
-        <div class="flex-1">
-            <div class="flex items-center justify-between">
-                <h3 class="font-black">{{ group.name }}</h3>
+    <!-- <div class="cursor-pointer hover:bg-gray-100 dark:hover:bg-slate-800"> -->
+    <div
+        class="dark:bg-slate-900 cursor-pointer rounded dark:text-gray-100 transition-all border-2 border-transparent hover:bg-white hover:shadow-xl hover:border-red-500"
+    >
+        <Link
+            :href="route('group.profile', group.slug)"
+            class="flex items-start gap-1 py-2 px-2"
+        >
+            <img :src="group.thumbnail_url" class="w-[32px] rounded-full" />
+            <div class="flex-1">
+                <div class="flex items-center justify-between">
+                    <h3 class="font-black">{{ group.name }}</h3>
+                </div>
+                <!-- <div class="text-xs text-gray-500" v-html="group.description"></div> -->
+                <span class="text-xs">
+                    {{
+                        group.status === "approved"
+                            ? group.role === "admin"
+                                ? group.role
+                                : ""
+                            : "not approved"
+                    }}
+                </span>
             </div>
-            <!-- <div class="text-xs text-gray-500" v-html="group.description"></div> -->
-            <span class="text-xs">
-                {{ group.status === 'approved' ? (group.role === 'admin' ? group.role : '') : 'not approved' }}
-            </span>
-        </div>
         </Link>
     </div>
 </template>
