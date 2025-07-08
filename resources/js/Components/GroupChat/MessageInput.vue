231ace6b232dd2157aa00ad4576969141d429532
ITDEPT-bb <itdept.bb88@gmail.com>
2025-07-08 11:14:40 +0800

    hotfix: next line when enter is pressed on group chat message

--- resources/js/Components/GroupChat/MessageInput.vue
+++ resources/js/Components/GroupChat/MessageInput.vue
@@ -114,7 +114,8 @@
             <!-- Textarea for message input -->
             <textarea
                 v-model="newMessage"
-                @keyup.enter="sendMessage"
+                @keydown.enter.exact.prevent="sendMessage"
+                @keydown.enter.shift.exact="addNewLine"
                 id="chat"
                 rows="1"
                 class="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -194,6 +195,11 @@ const selectedFiles = ref([]);
 const fileInput = ref(null);
 
 const showMobileActions = ref(false);
+
+const addNewLine = (event) => {
+    event.preventDefault();
+    newMessage.value += "\n";
+};
 
 function triggerFileInput() {
     fileInput.value.click();
