1a10c864b88c8aeb40b2505953bd3f747afb1bac
ITDEPT-bb <itdept.bb88@gmail.com>
2025-07-25 11:03:27 +0800

    hotfix: adding different files when sending attachments on chat

--- resources/js/Components/Chat/MessageList.vue
+++ resources/js/Components/Chat/MessageList.vue
@@ -86,14 +86,41 @@
                                 </div>
 
                                 <!-- Render other file types -->
-                                <div v-else>
-                                    <Link
+                                <div
+                                    v-else
+                                    class="flex items-center justify-between px-3 py-2 rounded-md bg-gray-50 dark:bg-gray-900 border border-gray-200 dark:border-gray-700"
+                                >
+                                    <!-- File name -->
+                                    <a
                                         :href="`/storage/${attachment.path}`"
                                         target="_blank"
-                                        class="text-blue-500 underline"
+                                        class="text-sm text-gray-800 dark:text-gray-200 hover:underline truncate max-w-[70%]"
                                     >
                                         {{ attachment.name }}
-                                    </Link>
+                                    </a>
+
+                                    <!-- Download icon -->
+                                    <a
+                                        :href="`/storage/${attachment.path}`"
+                                        :download="attachment.name"
+                                        class="text-gray-500 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white transition"
+                                        title="Download"
+                                    >
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            class="h-5 w-5"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke="currentColor"
+                                            stroke-width="1.5"
+                                        >
+                                            <path
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                d="M4 16v2a2 2 0 002 2h12a2 2 0 002-2v-2M7 10l5 5m0 0l5-5m-5 5V4"
+                                            />
+                                        </svg>
+                                    </a>
                                 </div>
                             </div>
                         </div>
--- resources/js/Components/GroupChat/MessageInput.vue
+++ resources/js/Components/GroupChat/MessageInput.vue
@@ -91,7 +91,7 @@
                     @change="handleFileChange"
                     class="hidden"
                     multiple
-                    accept="image/*, video/*"
+                    accept="*"
                 />
 
                 <button
@@ -142,7 +142,7 @@
                 :key="index"
                 class="relative"
             >
-                <img
+                <!-- <img
                     v-if="isImage(file)"
                     :src="filePreview(file)"
                     class="h-32 w-32 object-cover rounded-lg"
@@ -152,7 +152,33 @@
                     :src="filePreview(file)"
                     class="h-32 w-32 object-cover rounded-lg"
                     controls
+                /> -->
+                <img
+                    v-if="isImage(file)"
+                    :src="filePreview(file)"
+                    class="h-32 w-32 object-cover rounded-lg"
                 />
+                <video
+                    v-else-if="isVideo(file)"
+                    :src="filePreview(file)"
+                    class="h-32 w-32 object-cover rounded-lg"
+                    controls
+                />
+                <div
+                    v-else-if="isCDR(file)"
+                    class="h-32 w-32 flex flex-col items-center justify-center bg-gray-200 rounded-lg p-2 text-sm text-gray-600"
+                >
+                    <span>📄</span>
+                    <span class="text-xs text-center truncate w-full">{{
+                        file.name
+                    }}</span>
+                </div>
+                <div
+                    v-else
+                    class="h-32 w-32 flex items-center justify-center bg-gray-200 rounded-lg p-2 text-sm text-gray-600"
+                >
+                    <span>{{ file.name }}</span>
+                </div>
                 <button
                     @click="removeFile(index)"
                     class="absolute top-1 right-1 bg-gray-600 text-white p-1 rounded-full"
@@ -206,8 +232,17 @@ function triggerFileInput() {
 }
 
 function handleFileChange(event) {
+    const maxSize = 20 * 1024 * 1024;
     const files = Array.from(event.target.files);
-    selectedFiles.value.push(...files);
+    // selectedFiles.value.push(...files);
+
+    const validFiles = files.filter((file) => file.size <= maxSize);
+
+    if (validFiles.length < files.length) {
+        alert("Some files exceeded the 10MB limit and were skipped.");
+    }
+
+    selectedFiles.value.push(...validFiles);
 }
 
 function removeFile(index) {
@@ -220,6 +255,10 @@ function isImage(file) {
 
 function isVideo(file) {
     return file && file.type.startsWith("video/");
+}
+
+function isCDR(file) {
+    return file.name.endsWith(".cdr");
 }
 
 function filePreview(file) {
