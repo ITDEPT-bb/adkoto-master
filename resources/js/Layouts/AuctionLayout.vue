cb6cc148bfdd7f294f8396a2d8873e24d5a0d173
Ahllaine Christian De Ocera <acdeocera.dev@gmail.com>
2025-10-29 12:50:30 +0800

    feat: halloween update 2025 (2)

--- resources/js/Layouts/AuctionLayout.vue
+++ resources/js/Layouts/AuctionLayout.vue
@@ -37,11 +37,13 @@ import MothersDayIcon from "/public/img/mothers_day/2.png";
 import IconBackground from "/public/img/mothers_day/icon_bg.png";
 import AwardIcon from "/public/img/mothers_day/best_mom.png";
 import SearchBarIcon from "/public/img/mothers_day/icon.png";
-import ProfileSmallIcon from "/public/img/mothers_day/p1.png";
 
 import axiosClient from "@/axiosClient.js";
 import DarkModeToggle from "@/Components/DarkModeToggle.vue";
 import IncomingCallModal from "@/Components/Call/IncomingCallModal.vue";
+
+// Halloween Specific
+import ProfileSmallIcon from "/public/img/Halloween/hat.png";
 
 const showingNavigationDropdown = ref(false);
 const keywords = ref(usePage().props.search || "");
@@ -168,11 +170,11 @@ else if (
             class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700 fixed md:static top-0 z-10 pt-6 sm:pt-0 shadow"
         >
             <!-- Primary Navigation Menu -->
-            <!-- <div
+            <div
                 class="max-w-screen mx-auto px-4 py-0.5 sm:px-6 lg:px-20 bg-cover bg-center drop-shadow-lg relative sm:z-50 z-0"
-                style="background-image: url('/img/mothers_day/Bg.png')"
-            > -->
-            <div class="max-w-screen mx-auto px-4 sm:px-6 lg:px-16">
+                style="background-image: url('/img/Halloween/bg.jpg')"
+            >
+                <!-- <div class="max-w-screen mx-auto px-4 sm:px-6 lg:px-16"> -->
                 <!-- <div class="flex items-center justify-between gap-4 h-16">
                     <div class="flex"> -->
                 <!-- <div
@@ -627,15 +629,15 @@ else if (
                                                 :src="authUser.avatar_url"
                                                 :alt="authUser.name"
                                             />
-                                            <!-- <div
+                                            <div
                                                 class="absolute -bottom-1 xl:right-2 2xl:right-2 lg:block hidden"
                                             >
                                                 <img
                                                     :src="ProfileSmallIcon"
-                                                    class="w-3 relative bottom-9 left-2 h-auto"
+                                                    class="w-6 relative bottom-9 left-2 h-auto"
                                                     alt="Logo"
                                                 />
-                                            </div> -->
+                                            </div>
                                         </button>
                                         <div
                                             id="profile-tooltip"
